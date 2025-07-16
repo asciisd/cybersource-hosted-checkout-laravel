af5239a597997675d0652ec6183618ab557f6a98
aemaddin <aemaddin@gmail.com>
2025-07-16 20:36:07 +0300

    refactor: Add billing details to signed fields

--- src/Cybersource.php
+++ src/Cybersource.php
@@ -12,12 +12,10 @@ class Cybersource
         // $params['partner_solution_id'] = 'IGT4AWTG';
         $params['access_key'] = Config::get('cybersource.access_key');
         $params['profile_id'] = Config::get('cybersource.profile_id');
-        $params['transaction_uuid'] = (string) Str::uuid();
         $params['unsigned_field_names'] = '';
         $params['signed_date_time'] = gmdate("Y-m-d\TH:i:s\Z");
         $params['locale'] = $params['locale'] ?? 'en';
         $params['transaction_type'] = 'authorization, create_payment_token';
-        $params['reference_number'] = $params['transaction_uuid'];
 
         $params['signed_field_names'] = 'access_key,profile_id,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,transaction_type,reference_number,amount,currency,payment_method';
 
